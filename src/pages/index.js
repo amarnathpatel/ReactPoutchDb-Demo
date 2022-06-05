854576d982ddf0c5cbcfb808832e5d70f0b07485
Amar Nath <amanath@cisco.com>
2022-06-05 17:16:12 +0530

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import NotesList from '../components/notes-list';
+
+import { Link } from 'react-router-dom';
+
+class IndexPage extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        let notes = Object.values(props.notes)
+        notes.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+
+        this.state = { notes };
+    }
+
+    render() {
+        let { notes } = this.state;
+
+        if (!notes.length) {
+            return (
+                <div className="app-intro">
+                    <h2>Welcome to ReactNotes</h2>
+                    <p>You don't have any notes. <Link className="btn" to="/new">Get started!</Link></p>
+                    <p>All notes you create will be stored right here, on your browser.</p>
+                    <hr/>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <h2>Notes</h2>
+                <NotesList notes={this.state.notes}/>
+            </div>
+        )
+    }
+}
+
+export default IndexPage;
